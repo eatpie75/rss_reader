9c152a21d9caa5b5953c58fdddf6eb5e76d346a7
eatpie75 <eatpie75@gmail.com>
2015-04-15 20:04:39 -0700

    fix editing feeds

--- feeds/forms.py
+++ feeds/forms.py
@@ -9,5 +9,4 @@ class AddFeedForm(forms.Form):
 class EditFeedForm(forms.Form):
 	title=forms.CharField(max_length=500)
 	feed_url=forms.URLField()
-	site_url=forms.URLField()
 	category=forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
